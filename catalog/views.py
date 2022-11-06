8cfd14f304f810d73bb6f27cd9be3eb768e5775c
Kateryna <ekaterynadanilchenko@ukr.net>
2022-11-06 13:20:06 -0500

    rising_error_404

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import Question
 
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'catalog/detail.html', {'question': question})
