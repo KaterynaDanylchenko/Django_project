c2271a204271c531277eac13a92ae6cb0e0e21d0
Kateryna <ekaterynadanilchenko@ukr.net>
2022-11-09 08:22:37 -0500

    2nd_views_testing

--- catalog/views.py
+++ catalog/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+class DetailView(generic.DetailView):
+    ...
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
