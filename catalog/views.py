9cf7e350f5417b3607f01b9f0662e569618a8552
Kateryna <ekaterynadanilchenko@ukr.net>
2022-11-09 05:26:12 -0500

    results.html_adding_to_templates

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from django.http import HttpResponse
+from .models import Choice, Question
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'catalog/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('catalog:results', args=(question.id,)))
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'catalog/results.html', {'question': question})
 
 
 def index(request):
